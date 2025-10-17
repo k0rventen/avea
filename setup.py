a2c2e2eed427f57a62777ee8cea4f58309c01ba4
pattyland <mail@pattyland.de>
2025-10-17 16:00:47 +0200

    bump version to 1.6.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="avea",
-    version="1.5.2",
+    version="1.6.0",
     author="k0rventen",
     description="Control an Elgato Avea bulb using python3",
     long_description=long_description,
